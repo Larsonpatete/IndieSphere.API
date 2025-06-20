a14301b56f61c65c0b03b4de13e57cd2a9ce8835
Larson Patete <larsonpatete@hotmail.com>
2025-06-19 22:48:12 -0500

    Removed C# front end since I moved to react

--- IndieSphere/IndieSphere.AppHost/Program.cs
+++ IndieSphere/IndieSphere.AppHost/Program.cs
@@ -2,9 +2,5 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.IndieSphere_ApiService>("apiservice");
 
-builder.AddProject<Projects.IndieSphere_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
+// TODO: add react frontend project
 builder.Build().Run();
